4122153f2fcb885acc77a8e633d4bad65ee75c40
ajaykannan07 <ajaykannan176@gmail.com>
2024-10-04 15:13:15 +0530

    Create WaterContainer.java

---
+++ WaterContainer.java
@@ -0,0 +1,25 @@
+public class WaterContainer {
+    public static int maxArea(int[] height) {
+        int left = 0;  
+        int right = height.length - 1; 
+        int maxArea = 0;  
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]); 
+            int currentArea = width * currentHeight;
+            maxArea = Math.max(maxArea, currentArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }      
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        int[] height1 = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println("Maximum water container for height1: " +maxArea(height1));  // Output: 49
+        int[] height2 = {1, 1};
+        System.out.println("Maximum water container for height2: " +maxArea(height2));  // Output: 1
+    }
+}
